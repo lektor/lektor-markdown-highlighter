96ec9570750c41055d95cf053ea559b307a1fa9f
Armin Ronacher <armin.ronacher@active-4.com>
2015-12-25 00:06:53 +0100

    Added some missing things

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email='armin.ronacher@active-4.com',
     url='http://github.com/lektor/lektor-markdown-highlighter',
     version='0.1',
+    license='BSD',
     py_modules=['lektor_markdown_highlighter'],
     entry_points={
         'lektor.plugins': [
